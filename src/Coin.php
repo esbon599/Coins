1403d4aa50f5e24f8b5b9df05ba19ef62e7bda9e
Tom Mertz & Conor Baumgart <student@epicodus.com>
2015-03-10 14:24:19 -0700

    Add quarter test

--- src/Coin.php
+++ src/Coin.php
@@ -4,13 +4,21 @@
     {
         function coinAmount($change)
         {
-            $change_array = array();
+            $change_array = array(0,0,0,0);
+
+            $quarters = floor($change/25);
+            $change_array[3] = $quarters;
+            $change -= ($quarters * 25);
+
+            $dimes = floor($change/10);
+            $change_array[2] = $dimes;
+            $change -= ($dimes * 10);
 
             $nickles = floor($change / 5);
-            array_push($change_array, $nickles);
+            $change_array[1] = $nickles;
             $change -= ($nickles * 5);
 
-            array_push($change_array, $change / 1);
+            $change_array[0] = $change;
 
 
 
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -4,6 +4,11 @@
 
     class CoinTest extends PHPUnit_Framework_TestCase
     {
+        /*
+        input: 1
+        output: [1, 0, 0, 0]
+        Spec: Input an amount and output that number of pennies.
+        */
         function test_coinAmount_pennies()
         {
             //Arrange
@@ -14,9 +19,13 @@
             $result = $test_CoinAmount->coinAmount($input);
 
             //Assert
-            $this->assertEquals([0, 1], $result);
+            $this->assertEquals([1, 0, 0, 0], $result);
         }
-
+        /*
+        input: 5
+        output:[0, 1, 0, 0]
+        Spec: Input an amount and output the number of nickels.
+        */
         function test_coinAmount_nickels()
         {
             //Arrange
@@ -27,6 +36,79 @@
             $result = $test_CoinAmount->coinAmount($input);
 
             //Assert
-            $this->assertEquals([1, 0], $result);
+            $this->assertEquals([0, 1, 0, 0], $result);
         }
+
+        /*
+        input: 6
+        output: [1, 1, 0, 0]
+        Spec: input an amount and output the minimum number of nickels and pennies
+        */
+        function test_coinAmount_nickelPenny()
+        {
+            //Arrange
+            $test_Coin = new Coin;
+            $input = 6;
+
+            //Act
+            $result = $test_Coin->coinAmount($input);
+
+            //Assert
+            $this->assertEquals([1, 1, 0, 0], $result);
+        }
+
+        /*
+        input: 10
+        output: [0, 0, 1, 0]
+        Spec: input an amount and output the minimum number of dimes
+        */
+        function test_coinAmount_dime()
+        {
+            //Arrange
+            $test_Coin = new Coin;
+            $input = 10;
+
+            //Act
+            $result = $test_Coin->coinAmount($input);
+
+            //Assert
+            $this->assertEquals([0, 0, 1, 0], $result);
+        }
+
+        /*
+        input: 16
+        output: [1, 1, 1, 0]
+        Spec: input an amount and output the minimum number of dimes, nickels and pennies
+        */
+        function test_coinAmount_dimeNickelPenny()
+        {
+            //arrange
+            $test_Coin = new Coin;
+            $input = 16;
+
+            //act
+            $result = $test_Coin->coinAmount($input);
+
+            //assert
+            $this->assertEquals([1, 1, 1, 0], $result);
+        }
+
+        /*
+        input: 25
+        output: [0, 0, 0, 1]
+        Spec: input an amount and output the minimum number of quarters
+        */
+        function test_coinAmount_quarters()
+        {
+            //arrange
+            $test_Coin = new Coin;
+            $input = 25;
+
+            //act
+            $result = $test_Coin->coinAmount($input);
+
+            //assert
+            $this->assertEquals([0, 0, 0, 1], $result);
+        }
+
     }
