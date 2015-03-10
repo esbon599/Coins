a97c5b9d1b4a4af8849d0868e7bc4a8f4d48cb60
Tom Mertz & Conor Baumgart <student@epicodus.com>
2015-03-10 12:28:16 -0700

    Add pennies test

--- src/Coin.php
+++ src/Coin.php
@@ -4,6 +4,12 @@
     {
         function coinAmount($change)
         {
+            $change_array = array();
+
+
+                array_push($change_array, $change / 1);
             
+            return $change_array;
         }
     }
+?>
