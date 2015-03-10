eefc6cc1ab4cc8e4e8ad722dd3b9c6c7083db274
Tom Mertz & Conor Baumgart <student@epicodus.com>
2015-03-10 15:16:32 -0700

    Add results page

--- app/app.php
+++ app/app.php
@@ -4,9 +4,19 @@
     require_once __DIR__."/../src/Coin.php";
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
-    $app->get("/", function() {
-        return "hay hay hay";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.twig');
+    });
+
+    $app->post("/change", function() use ($app) {
+        $coin = new Coin();
+        $coin_array = $coin->coinAmount($_POST['change']);
+        return $app['twig']->render('result.twig', array('coins' => $coin_array));
+
     });
 
     return $app;
